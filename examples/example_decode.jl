4ae87f814fcd61897d24126c86c4b9259b135657
Alexander Barth <barth.alexander@gmail.com>
2019-08-16 20:23:52 +0200

    wip

---
+++ examples/example_decode.jl
@@ -0,0 +1,38 @@
+using FileIO
+using Statistics
+import APTDecoder
+using PyPlot
+
+wavname = "/home/abarth/src/APTDecoder/test.wav"
+wavname = "/mnt/data1/abarth/Backup/abarth/testapt/gqrx_20180715_150114_137100000.wav"
+
+#wavname = "/home/abarth/testapt/gqrx_20180715_150114_137100000.wav"
+wavname = "/home/abarth/gqrx_20190804_141523_137100000.wav"
+wavname = "/home/abarth/testapt/gqrx_20180715_150114_137100000.wav"
+wavname = "/home/abarth/gqrx_20190814_192855_137917500.wav"
+wavname = "gqrx_20190804_141523_137100000.wav"
+
+y,Fs,nbits,opt = load(wavname)
+
+datatime,(channelA,channelB),data = APTDecoder.decode(y,Fs)
+
+vmin,vmax = quantile(view(data,:),[0.01,0.99])
+data[data .> vmax] .= vmax;
+data[data .< vmin] .= vmin;
+
+#=
+cmap = "RdYlBu_r"
+subplot(1,2,1);
+imshow(channelA, aspect="auto", cmap=cmap); colorbar();
+title("Channel A")
+
+subplot(1,2,2);
+imshow(channelB, aspect="auto", cmap=cmap); colorbar();
+title("Channel B")
+=#
+
+starttime = APTDecoder.starttimename(wavname)
+
+Alon,Alat,Adata = APTDecoder.georeference(channelA,satellite_name,datatime,starttime)
+figure("Channel a - geo")
+APTDecoder.plot(Alon,Alat,Adata)
