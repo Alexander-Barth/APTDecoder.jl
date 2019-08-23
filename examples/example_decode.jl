df2573c6d8c22c624bd69b6623584441fbbf1930
Alexander Barth <barth.alexander@gmail.com>
2019-08-23 22:26:39 +0200

    clean-up

--- examples/example_decode.jl
+++ examples/example_decode.jl
@@ -4,47 +4,28 @@ using Statistics
 using PyPlot
 using WAV
 
-wavname = "/home/abarth/src/APTDecoder/test.wav"
-wavname = "/mnt/data1/abarth/Backup/abarth/testapt/gqrx_20180715_150114_137100000.wav"
-
-#wavname = "/home/abarth/testapt/gqrx_20180715_150114_137100000.wav"
-wavname = "/home/abarth/gqrx_20190804_141523_137100000.wav"
-wavname = "/home/abarth/testapt/gqrx_20180715_150114_137100000.wav"
-wavname = "/home/abarth/gqrx_20190814_192855_137917500.wav"
-wavname = "gqrx_20190804_141523_137100000.wav"
+# file name as recorde by gqrx
 wavname = "gqrx_20190823_173900_137620000.wav"
 
-satellite_name = "NOAA 19"
+# name of the satellite, as used by www.celestrak.com
 satellite_name = "NOAA 15"
 
 # satellite orbit information (TLE)
+# https://en.wikipedia.org/wiki/Two-line_element_set
 tles = SatelliteToolbox.read_tle("weather-20190823.txt")
 
+# load the wav file
 y,Fs,nbits,opt = wavread(wavname)
 
+# decode to image
 datatime,(channelA,channelB),data = APTDecoder.decode(y,Fs)
 
 vmin,vmax = quantile(view(data,:),[0.01,0.99])
 data[data .> vmax] .= vmax;
 data[data .< vmin] .= vmin;
 
-#=
-cmap = "RdYlBu_r"
-subplot(1,2,1);
-imshow(channelA, aspect="auto", cmap=cmap); colorbar();
-title("Channel A")
-
-subplot(1,2,2);
-imshow(channelB, aspect="auto", cmap=cmap); colorbar();
-title("Channel B")
-=#
-
-
 figure("APTDecoder")
 cmap = "RdYlBu_r"
-#subplot(2,1,1);
-#pcolormesh(reverse(reverse(data,dims=1),dims=2))
-#colorbar()
 subplot(2,1,1);
 imshow(reverse(reverse(data,dims=1),dims=2), aspect="auto", cmap=cmap);
 title("Raw data")
@@ -59,20 +40,7 @@ title("Channel A")
 
 Blon,Blat,Bdata = APTDecoder.georeference(channelB,satellite_name,datatime,starttime; tles=tles)
 subplot(2,2,4)
-#figure("Channel B - geo")
 APTDecoder.plot(Blon,Blat,Bdata)
 title("Channel B")
 
-
-#=
-starttime = APTDecoder.starttimename(wavname)
-
-# TLEs are downloaded if omited
-Alon,Alat,Adata = APTDecoder.georeference(channelA,satellite_name,datatime,starttime; tles=tles)
-figure("Channel A - geo")
-APTDecoder.plot(Alon,Alat,Adata)
-
-Blon,Blat,Bdata = APTDecoder.georeference(channelB,satellite_name,datatime,starttime; tles=tles)
-figure("Channel B - geo")
-APTDecoder.plot(Blon,Blat,Bdata)
-=#
+savefig("example_decode.png")
