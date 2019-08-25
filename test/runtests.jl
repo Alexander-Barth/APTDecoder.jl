7d9dd1a74892ea8e2cd2ce772a450fd802da10ec
Alexander Barth <barth.alexander@gmail.com>
2019-08-25 23:23:37 +0200

    list Test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,8 @@
 import APTDecoder
 using Test
 
+
+wavname = download("https://archive.org/download/gqrx_20190825_182745_137620000/gqrx_20190825_182745_137620000.wav")
+APTDecoder.makeplots(wavname,"NOAA 15")
+
 @test 1 == 1
