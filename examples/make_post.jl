2752cced34b186268ab5ff63bb06489fff668ef9
Alexander Barth <barth@alexander@gmail.com>
2023-12-25 17:12:57 +0100

    fix ActivityPub post

--- examples/make_post.jl
+++ examples/make_post.jl
@@ -10,7 +10,7 @@ function publish(config,message,fnames; mime_type = "image/png")
    media_ids = [ActivityPub.post_media(conn,fname,mime_type,
 	                                    description = basename(fname)) for fname in fnames]
 				  
-   response = ActivityPub.post_status(conn,status, media_ids = media_ids)
+   response = ActivityPub.post_status(conn,message, media_ids = media_ids)
    return nothing
 end
 
